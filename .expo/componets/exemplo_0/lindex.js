f0c50b1d00759b6837ac4294fc784a11cc68daa1
raphaelalessandro <160646952+raphaelalessandro@users.noreply.github.com>
2024-02-27 15:24:23 -0300

    aula 27-02

---
+++ .expo/componets/exemplo_0/lindex.js
@@ -0,0 +1,11 @@
+import{View, Text} from  'react-natuive';
+
+function exemplo_0 () {
+    return(
+     <View>
+        <text>  Minha primeira tela </text>
+     </View>
+    );
+}
+
+export default exemplo_0;
