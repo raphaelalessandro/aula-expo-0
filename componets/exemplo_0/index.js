11429223c558c66a121663854886da02921b39dd
raphaelalessandro <160646952+raphaelalessandro@users.noreply.github.com>
2024-03-01 09:48:34 -0300

    aula 01-03

---
+++ componets/exemplo_0/index.js
@@ -0,0 +1,11 @@
+import {View, Text} from  'react-native';
+
+function Exemplo_0 () {
+    return(
+     <View>
+        <Text style={{color: '#fff'}}>  Minha primeira tela </Text>
+     </View>
+    );
+}
+
+export default Exemplo_0;
