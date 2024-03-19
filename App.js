8a2f7d8b3992cc7cffd2e22d966901c5d2b315f0
raphaelalessandro <160646952+raphaelalessandro@users.noreply.github.com>
2024-03-19 15:23:24 -0300

    aula 19-03

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Exemplo_0 from './componets/exemplo_1'
+import Exemplo_2 from './componets/exemplo_2'
 
 
 import Atividade_1 from './componets/atividade_1';
@@ -9,7 +10,7 @@ import Atividade_1 from './componets/atividade_1';
 export default function App() {
   return (
     <View style={styles.componets}>
-      <Atividade_1 />
+      <Exemplo_2 />
       <StatusBar style="auto" />
     </View>
   );
---
+++ componets/exemplo_2/index.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+
+import styles from './styles';
+
+function Exemplo2 () {
+    return(
+        <View style={styles.container}>
+            <text>Exemplo2</text>
+        </View>
+    )
+} 
+
+export default Exemplo2;
